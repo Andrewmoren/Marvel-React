c614db2191f9709929a1f5f7922bc05ce4853d3c
Andrew <andrewmoren93@gmail.com>
2022-07-26 12:58:52 +0300

    add json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+
+import "./style/style.scss";
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then((res) => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
