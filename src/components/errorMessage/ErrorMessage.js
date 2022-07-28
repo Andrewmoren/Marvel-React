a69d65d3ba085a2a5b91d793e1cde65c0a5ba177
Andrew <andrewmoren93@gmail.com>
2022-07-28 18:03:52 +0300

    add timerID

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,19 @@
+import img from "./error.gif";
+
+const ErrorMessage = () => {
+  return (
+    <img
+      style={{
+        display: "block",
+        width: "250px",
+        height: "250px",
+        objectFit: "contain",
+        margin: "0 auto",
+      }}
+      src={img}
+      alr="Error"
+    />
+  );
+};
+
+export default ErrorMessage;
